923d3856d15d084f092a07f2c3c4ee5381350318
Eric Barnes <barnese@gmail.com>
2016-07-08 09:23:07 -0600

    Initial commit. Added DocumentDistanceFinder.

---
+++ EBAlgorithms/DocumentDistanceFinder.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBAlgorithms
+{
+    public class DocumentDistanceFinder
+    {
+        /// <summary>
+        /// Finds the distance between two files, where the distance is how many words are shared between the files.
+        /// Converts the files into vectors of words and returns the distance as the angle between the two using their dot product.
+        /// </summary>
+        /// <returns>Distance as a decimal ranging from 0 to pi/2.</returns>
+        public double FindDistance(string fileName1, string fileName2) {
+            var words1 = SplitFileIntoWords(fileName1);
+            var words2 = SplitFileIntoWords(fileName2);
+
+            var wordFrequencies1 = CountWordFrequencies(words1);
+            var wordFrequencies2 = CountWordFrequencies(words2);
+
+            return CalculateVectorAngle(wordFrequencies1, wordFrequencies2);
+        }
+
+        private long CalculateDotProduct(Dictionary<string, int> d1, Dictionary<string, int> d2) {
+            // Dot product of d1 and d2 = a1 * b1 + a2 * b2 + ... + an * bn.
+            long sum = 0;
+
+            foreach (var entry1 in d1) {
+                if (d2.ContainsKey(entry1.Key))
+                    sum += entry1.Value * d2[entry1.Key];
+            }
+
+            return sum;
+        }
+
+        private double CalculateVectorAngle(Dictionary<string, int> d1, Dictionary<string, int> d2) {
+            // Angle between two vectors: cos theta = (d1 * d2) / (|d1| * |d2|)
+            // Magnitude of a vector: |d1| = sqroot(a1 ^ 2 + a2 ^ 2 + ... + an ^ 2)
+
+            double product = CalculateDotProduct(d1, d2);
+            double magnitudes = Math.Sqrt(CalculateDotProduct(d1, d1) * CalculateDotProduct(d2, d2));
+                
+            return Math.Acos(product / magnitudes);
+        }
+
+        private Dictionary<string, int> CountWordFrequencies(string[] words) {
+            var wordFrequencies = new Dictionary<string, int>();
+
+            foreach (var word in words) {
+                if (wordFrequencies.ContainsKey(word)) {
+                    wordFrequencies[word] = wordFrequencies[word] + 1;
+                } else {
+                    wordFrequencies[word] = 1;
+                }
+            }
+
+            return wordFrequencies;
+        }
+
+        private string ReadFileContents(string fileName) {
+            var reader = new StreamReader(fileName, Encoding.UTF8);
+            return reader.ReadToEnd();
+        }
+
+        private string[] SplitFileIntoWords(string fileName) {
+            var fileContents = ReadFileContents(fileName);
+
+            // Convert non-alphanumeric characters into spaces.
+            var characters = fileContents.ToCharArray();
+
+            for (int i = 0; i < characters.Length; i++) {
+                if (!Char.IsLetterOrDigit(characters[i])) {
+                    characters[i] = ' ';
+                }
+            }
+
+            return new String(characters).Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
---
+++ EBAlgorithmsConsole/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EBAlgorithms;
+
+namespace EBAlgorithmsConsole {
+    class Program {
+        private static string programName = System.AppDomain.CurrentDomain.FriendlyName;
+
+        static void Main(string[] args) {
+            if (args.Length == 0) {
+                Console.WriteLine("Usage: {0} <command> <parameters>", programName);
+                return;
+            }
+
+            var command = args[0].ToLower();
+
+            try {
+                switch (command) {
+                    case "distance":
+                        DocumentDistanceRunner(args);
+                        break;
+                    default:
+                        Console.WriteLine("Command not recognized.");
+                        break;
+                }
+            } catch (Exception exception) {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        /// <summary>
+        /// Runs the document distance algorithm.
+        /// </summary>
+        /// <example>
+        /// .\EBAlgorithmsConsole.exe distance ..\..\Data\DistanceData1.txt ..\..\Data\DistanceData2.txt
+        /// </example>
+        static void DocumentDistanceRunner(string[] args) {
+            if (args.Length != 3) {
+                Console.WriteLine("Usage: {0} distance filename1 filename2", programName);
+                return;
+            }
+
+            var documentDistanceFinder = new DocumentDistanceFinder();
+
+            var distance = documentDistanceFinder.FindDistance(args[1], args[2]);
+
+            Console.WriteLine("The distance is {0} (radians)", distance);
+
+            var ratio = distance / (Math.PI / 2);
+
+            Console.WriteLine("Difference ratio is {0:N2}", ratio);
+        }
+    }
+}
