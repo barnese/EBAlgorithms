1838f7fefb0229d63b4d597543d544ac0ce3eff6
Eric Barnes <barnese@gmail.com>
2016-07-08 10:00:41 -0600

    Added unit test project

---
+++ EBAlgorithmsUnitTests/DocumentDistanceFinderTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EBAlgorithms;
+
+namespace EBAlgorithmsUnitTests {
+    [TestClass]
+    public class DocumentDistanceFinderTests {
+        private string file1 = "../../Data/DistanceData1.txt";
+        private string file2 = "../../Data/DistanceData2.txt";
+
+        [TestMethod]
+        public void DocumentDistanceFinder_FindDifferingDistance() {
+            var documentDistanceFinder = new DocumentDistanceFinder();
+
+            var distance = documentDistanceFinder.FindDistance(file1, file2);
+
+            // Since the files are completely different, we expect the greatest distance: pi/2.
+            Assert.AreEqual(distance, Math.PI / 2);
+        }
+
+        [TestMethod]
+        public void DocumentDistanceFinder_FindIdenticalDistance() {
+            var documentDistanceFinder = new DocumentDistanceFinder();
+
+            var distance = documentDistanceFinder.FindDistance(file1, file1);
+
+            // Since the files are the same, we expect no distance.
+            Assert.AreEqual(distance, 0);
+        }
+    }
+}
