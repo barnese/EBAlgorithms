4fa47abea3fb4f8e34981c33a042da604f43dbcb
Eric Barnes <barnese@gmail.com>
2016-07-23 13:50:58 -0600

    Added counting sort unit tests

---
+++ UnitTests/AlgorithmTests/CountingSortTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+using EBAlgorithms;
+
+namespace EBAlgorithmsUnitTests {
+    public class CountingSortTests {
+
+        [Fact]
+        public void CountingSort_Integers() {
+            var list = TestData.Numbers;
+
+            list.CountingSort();
+
+            Assert.True(list.IsSorted());
+        }
+
+        [Fact]
+        public void CountingSort_Strings() {
+            var list = TestData.Words;
+
+            list.CountingSort();
+
+            Assert.True(list.IsSorted());
+        }
+    }
+}
